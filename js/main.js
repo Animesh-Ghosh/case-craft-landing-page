7ea59909c9041a0e9087b863086a7250e054c4d8
Animesh-Ghosh <animesh00914902017@msi-ggsip.org>
2020-08-11 14:45:09 +0530

    Add hidden navbar

--- js/main.js
+++ js/main.js
@@ -1,25 +1,44 @@
 (async function() {
   console.log('CaseCraft ready!');
-  const circle = await (async function() {
+  // get elements
+  const { hamburgerCircle, crossCircle } = await (async function() {
     return new Promise((resolve) => {
       window.onload = (event) => {
-        resolve(
-          document.getElementById('hamburger')
+        resolve({
+          hamburgerCircle: document.getElementById('hamburger')
+          .contentDocument.getElementById('circle'),
+          crossCircle: document.getElementById('cross')
           .contentDocument.getElementById('circle')
-        )
+        })
       }
     })
   })()
+  const hiddenMenu = document.getElementById('hidden')
+
+  // get colours
   const bgColor = 'rgba(246, 246, 249)'
-  const fillColor = circle.style.fill
-  circle.style.fill = bgColor
-  circle.style.stroke = bgColor
-  circle.addEventListener('mouseover', event => {
-    circle.style.fill = fillColor
-    circle.style.stroke = fillColor
+  const fillColor = hamburgerCircle.style.fill
+
+  // set default colours
+  hamburgerCircle.style.fill = bgColor
+  hamburgerCircle.style.stroke = bgColor
+
+  // event listeners
+  // on hover hamburger
+  hamburgerCircle.addEventListener('mouseover', event => {
+    hamburgerCircle.style.fill = fillColor
+    hamburgerCircle.style.stroke = fillColor
   })
-  circle.addEventListener('mouseout', event => {
-    circle.style.fill = bgColor
-    circle.style.stroke = bgColor
+  hamburgerCircle.addEventListener('mouseout', event => {
+    hamburgerCircle.style.fill = bgColor
+    hamburgerCircle.style.stroke = bgColor
+  })
+  // show hidden menu
+  hamburgerCircle.addEventListener('click', event => {
+    hiddenMenu.classList.add('show')
+  })
+  // hide hidden menu
+  crossCircle.addEventListener('click', event => {
+    hiddenMenu.classList.remove('show')
   })
 })()
