a6882b91b3d7485768b5a1f7b58eec6c36944cdc
Animesh-Ghosh <animesh00914902017@msi-ggsip.org>
2020-08-12 21:10:38 +0530

    Add hidden menu transition
    Added transition to hidden menu nav links.
    Added arrow SVG.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 (async function() {
   console.log('CaseCraft ready!');
   // get elements
-  const { hamburgerCircle, crossCircle } = await (async function() {
+  const { hamburgerCircle, crossCircle, arrows } = await (async function() {
     return new Promise((resolve) => {
       window.onload = (event) => {
+        let arrows = Array.from(document.getElementsByClassName('arrow'))
+        arrows = arrows.map(arrow => arrow.contentDocument.getElementById('arrow'))
         resolve({
           hamburgerCircle: document.getElementById('hamburger')
-          .contentDocument.getElementById('circle'),
+            .contentDocument.getElementById('circle'),
           crossCircle: document.getElementById('cross')
-          .contentDocument.getElementById('circle')
+            .contentDocument.getElementById('circle'),
+          arrows
         })
       }
     })
   })()
   const hiddenMenu = document.getElementById('hidden')
+  const hiddenNavLinks = document.querySelectorAll('#hidden-nav > nav > ul > li > a')
 
   // get colours
   const bgColor = 'rgba(246, 246, 249)'
@@ -35,10 +39,23 @@
   })
   // show hidden menu
   hamburgerCircle.addEventListener('click', event => {
+    hiddenMenu.classList.remove('hidden')
     hiddenMenu.classList.add('show')
   })
   // hide hidden menu
   crossCircle.addEventListener('click', event => {
     hiddenMenu.classList.remove('show')
+    hiddenMenu.classList.add('hidden')
+  })
+
+  const arrowStrokeColor = 'rgb(201, 199, 210)'
+  const primaryColor = 'rgb(150, 114, 251)'
+  hiddenNavLinks.forEach((hiddenNavLink, idx) => {
+    hiddenNavLink.addEventListener('mouseover', event => {
+      arrows[idx].style.stroke = primaryColor
+    })
+    hiddenNavLink.addEventListener('mouseout', event => {
+      arrows[idx].style.stroke = arrowStrokeColor
+    })
   })
 })()
